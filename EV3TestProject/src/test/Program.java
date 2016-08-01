3475b4c45bc9cf7cab626e8210659d37a4c03882
mhofer4991 <mhofer4991@gmail.com>
2016-08-01 11:08:54 +0200

    Delay is very important

--- EV3TestProject/src/test/Program.java
+++ EV3TestProject/src/test/Program.java
@@ -1,12 +1,14 @@
 package test;
 
 import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LCD.drawString("hello world!", 20, 20);
+		LCD.drawString("hello world!", 0, 0);
+		Delay.msDelay(5000);
 	}
 
 }
