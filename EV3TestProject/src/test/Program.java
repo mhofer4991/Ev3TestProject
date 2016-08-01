98c22b6b4507ea1105eadfea94018a93ca8f5cfe
mhofer4991 <mhofer4991@gmail.com>
2016-08-01 09:59:48 +0200

    test test test test

---
+++ EV3TestProject/src/test/Program.java
@@ -0,0 +1,12 @@
+package test;
+
+import lejos.hardware.lcd.LCD;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LCD.drawString("hello world!", 20, 20);
+	}
+
+}
