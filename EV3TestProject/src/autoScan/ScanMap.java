ea4606a59906b3ee975b862d2b8f54074ced342e
Michael Ploy <93216@fhwn.ac.at>
2016-08-04 16:37:16 +0200

    Changes to ScanAlgorithm and ScanMap

--- EV3TestProject/src/autoScan/ScanMap.java
+++ EV3TestProject/src/autoScan/ScanMap.java
@@ -13,13 +13,57 @@ public class ScanMap {
 	
 	public void AddScanResult(float direction, float freeDistance, Position startPosition)
 	{
-		// TODO: Checken um wieviel das Array zu klein ist	
-		// Differenz zwischen größtem x und y bei scan und array
-		// TODO: Array entsprechend vergrößern
-		// mittels differnz entsprechend vergrößern
+		int minX = startPosition.Get_X();
+		int minY = startPosition.Get_Y();
+		
+		// TODO: Distanzen in X und Y richtig berechnen
+		int distanceX = (int)(freeDistance * Math.sin(direction));
+		int distanceY = (int)(freeDistance * Math.cos(direction));
+		
+		int maxX = (startPosition.Get_X() + distanceX);
+		int maxY = (startPosition.Get_Y() + distanceY);
+				
+		// Checken um wieviel das Array zu klein ist	
+		// Differenz zwischen größtem x und y bei scan und array		
+		int posX = this.map.Get_Fields()[this.map.Get_Fields().length][0].Get_Position().Get_X() - maxX;
+		int negX = this.map.Get_Fields()[0][0].Get_Position().Get_X() - minX;
+		int posY = this.map.Get_Fields()[0][this.map.Get_Fields()[0].length].Get_Position().Get_Y() - maxY;
+		int negY = this.map.Get_Fields()[0][0].Get_Position().Get_Y() - minY;
+		
+		// Array entsprechend vergrößern
+		// mittels differnz entsprechend vergrößern		
+		if (posX > 0)
+		{
+			this.Extend(0, posX);
+		}
+		
+		if (posY > 0)
+		{
+			this.Extend(1, posY);
+		}
+		
+		if (negX > 0)
+		{
+			this.Extend(2, negX);
+		}
+		
+		if (negY > 0)
+		{
+			this.Extend(3, negY);
+		}
 		
 		// TODO: Alle gescannten Zellen auf free setzen
 		// Alle Zellen durchgehen, und die die gescannt wurden(Linie geht durch) auf free setzen
+		for (int i = 0; i < this.map.Get_Fields().length; i++)
+		{
+			for (int j = 0; j < this.map.Get_Fields()[0].length; j++)
+			{
+				if ((this.LineMeetsField(minX, minY, maxX, maxY, i, j)) && (this.map.Get_Fields()[i][j].Get_State() == Fieldstate.unscanned))
+				{
+					this.map.Get_Fields()[i][j].Set_State(Fieldstate.free);
+				}
+			}
+		}		
 	}
 	
 	// Direction: 0 - up, 1 - right, 2 - down, 3 - left
@@ -138,6 +182,16 @@ public class ScanMap {
 		this.map.Set_Fields(next);
 	}
 	
+	public boolean LineMeetsField(float lineXStart, float lineYStart, float lineXEnd, float lineYEnd, int fieldIndexX, int fieldIndexY)
+	{
+		float squareXStart = this.map.Get_Fields()[fieldIndexX][fieldIndexY].Get_Position().Get_X();
+		float squareYStart = this.map.Get_Fields()[fieldIndexX][fieldIndexY].Get_Position().Get_Y();
+		float squareXEnd = squareXStart +1;
+		float squareYEnd = squareYStart +1;
+		
+		return LineIntersectsSquare(lineXStart, lineYStart, lineXEnd, lineYEnd, squareXStart, squareYStart, squareXEnd, squareYEnd);
+	}
+	
 	// Start is the Bottom Left Position of the Line/Square, End is the Top Right Position of the Line/Square
 	public static boolean LineIntersectsSquare (float lineXStart, float lineYStart, float lineXEnd, float lineYEnd, float squareXStart, float squareYStart, float squareXEnd, float squareYEnd)
 	{
@@ -148,7 +202,7 @@ public class ScanMap {
 		}
 		
 		// Start or end inside.
-		if ((lineXStart > squareXStart && lineXStart < squareXEnd && lineYStart > squareYStart && lineYStart < squareYEnd) || (lineXEnd > squareXStart && lineXEnd < squareXEnd && lineYEnd > squareYStart && lineYEnd < squareYEnd)) 
+		if ((lineXStart >= squareXStart && lineXStart <= squareXEnd && lineYStart >= squareYStart && lineYStart <= squareYEnd) || (lineXEnd >= squareXStart && lineXEnd <= squareXEnd && lineYEnd >= squareYStart && lineYEnd <= squareYEnd)) 
 		{
 			return true;
 		}
@@ -157,16 +211,28 @@ public class ScanMap {
 		float m = (lineYEnd - lineYStart) / (lineXEnd - lineXStart);
 
 	    float y = m * (squareXStart - lineXStart) + lineYStart;
-	    if (y > squareYStart && y < squareYEnd) return true;
+	    if (y >= squareYStart && y <= squareYEnd)
+	    { 
+	    	return true;
+	    }
 
 	    y = m * (squareXEnd - lineXStart) + lineYStart;
-	    if (y > squareYStart && y < squareYEnd) return true;
+	    if (y >= squareYStart && y <= squareYEnd)
+	    { 
+	    	return true;
+	    }
 
 	    float x = (squareYStart - lineYStart) / m + lineXStart;
-	    if (x > squareXStart && x < squareXEnd) return true;
+	    if (x >= squareXStart && x <= squareXEnd)
+	    { 
+	    	return true;
+	    }
 
 	    x = (squareYEnd - lineYStart) / m + lineXStart;
-	    if (x > squareXStart && x < squareXEnd) return true;		
+	    if (x >= squareXStart && x <= squareXEnd)
+	    {
+	    	return true;
+	    }		
 		
 		return false;
 	}
