706112a454cdfa38c9bc5794311037c98ad20d49
Michael Ploy <93216@fhwn.ac.at>
2016-08-04 10:06:16 +0200

    Added Line Square Intersection Check

--- EV3TestProject/src/autoScan/ScanAlgorithm.java
+++ EV3TestProject/src/autoScan/ScanAlgorithm.java
@@ -62,7 +62,7 @@ public class ScanAlgorithm {
 	}
 	
 	public boolean abort;
-	
+
 	public static void Draw(Map map)
 	{
 		Field[][] fields =  map.Get_Fields();
@@ -84,4 +84,37 @@ public class ScanAlgorithm {
 		System.out.println();
 		System.out.println();
 	}	  
+
+	// Start is the Bottom Left Position of the Line/Square, End is the Top Right Position of the Line/Square
+	public static boolean LineIntersectsSquare (float lineXStart, float lineYStart, float lineXEnd, float lineYEnd, float squareXStart, float squareYStart, float squareXEnd, float squareYEnd)
+	{
+		// Prüfen ob das Quadrat im rechteck aus start und endpunkt der line liegt
+		if ((lineXStart < squareXStart && lineXEnd < squareXStart) || (lineYStart < squareYStart && lineYEnd < squareYStart) || (lineXStart > squareXEnd && lineXEnd > squareXEnd) || (lineYStart > squareYEnd && lineYEnd > squareYEnd))
+		{
+			return false;
+		}
+		
+		// Start or end inside.
+		if ((lineXStart > squareXStart && lineXStart < squareXEnd && lineYStart > squareYStart && lineYStart < squareYEnd) || (lineXEnd > squareXStart && lineXEnd < squareXEnd && lineYEnd > squareYStart && lineYEnd < squareYEnd)) 
+		{
+			return true;
+		}
+		
+		// Steigung der Linie berechnen
+		float m = (lineYEnd - lineYStart) / (lineXEnd - lineXStart);
+
+	    float y = m * (squareXStart - lineXStart) + lineYStart;
+	    if (y > squareYStart && y < squareYEnd) return true;
+
+	    y = m * (squareXEnd - lineXStart) + lineYStart;
+	    if (y > squareYStart && y < squareYEnd) return true;
+
+	    float x = (squareYStart - lineYStart) / m + lineXStart;
+	    if (x > squareXStart && x < squareXEnd) return true;
+
+	    x = (squareYEnd - lineYStart) / m + lineXStart;
+	    if (x > squareXStart && x < squareXEnd) return true;		
+		
+		return false;
+	}
 }
