cdf916a3ab7a22f0952b57d85446d052f007b062
Michael Ploy <93216@fhwn.ac.at>
2016-08-04 12:09:13 +0200

    Changes to AutoscanAlgorithm and ScanResultAdding

--- EV3TestProject/src/autoScan/ScanAlgorithm.java
+++ EV3TestProject/src/autoScan/ScanAlgorithm.java
@@ -61,8 +61,32 @@ public class ScanAlgorithm {
 		Draw(sm.map);
 	}
 	
+	public ScanAlgorithm(Map map)
+	{
+		this.abort = false;
+	}
+	
 	public boolean abort;
 
+	public void Scan()
+	{
+		this.abort = false;
+		
+		while (!this.abort)
+		{
+			// TODO: Current Field auf free&scanned setzen
+			
+			// TODO: Rundumscan
+			// TODO: Scan in die Richtungen wo unscanned oder free ist
+						
+			// TODO: Liste der free Felder
+			// TODO: Liste der anfahrbaren free Felder
+			// TODO: Falls keine freien, ungescannten mehr anfahrbar -> break
+			
+			// TODO: Nächstes freies, ungescanntes Feld anfahren
+		}		
+	}
+	
 	public static void Draw(Map map)
 	{
 		Field[][] fields =  map.Get_Fields();
@@ -83,38 +107,5 @@ public class ScanAlgorithm {
 		
 		System.out.println();
 		System.out.println();
-	}	  
-
-	// Start is the Bottom Left Position of the Line/Square, End is the Top Right Position of the Line/Square
-	public static boolean LineIntersectsSquare (float lineXStart, float lineYStart, float lineXEnd, float lineYEnd, float squareXStart, float squareYStart, float squareXEnd, float squareYEnd)
-	{
-		// Prüfen ob das Quadrat im rechteck aus start und endpunkt der line liegt
-		if ((lineXStart < squareXStart && lineXEnd < squareXStart) || (lineYStart < squareYStart && lineYEnd < squareYStart) || (lineXStart > squareXEnd && lineXEnd > squareXEnd) || (lineYStart > squareYEnd && lineYEnd > squareYEnd))
-		{
-			return false;
-		}
-		
-		// Start or end inside.
-		if ((lineXStart > squareXStart && lineXStart < squareXEnd && lineYStart > squareYStart && lineYStart < squareYEnd) || (lineXEnd > squareXStart && lineXEnd < squareXEnd && lineYEnd > squareYStart && lineYEnd < squareYEnd)) 
-		{
-			return true;
-		}
-		
-		// Steigung der Linie berechnen
-		float m = (lineYEnd - lineYStart) / (lineXEnd - lineXStart);
-
-	    float y = m * (squareXStart - lineXStart) + lineYStart;
-	    if (y > squareYStart && y < squareYEnd) return true;
-
-	    y = m * (squareXEnd - lineXStart) + lineYStart;
-	    if (y > squareYStart && y < squareYEnd) return true;
-
-	    float x = (squareYStart - lineYStart) / m + lineXStart;
-	    if (x > squareXStart && x < squareXEnd) return true;
-
-	    x = (squareYEnd - lineYStart) / m + lineXStart;
-	    if (x > squareXStart && x < squareXEnd) return true;		
-		
-		return false;
-	}
+	}	  	
 }
